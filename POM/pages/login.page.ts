299c3b39d4ae54c0ebd82c572ad00f9bb7406319
Pavlo Sukiasyan <pavlo.sukiasyan@gmail.com>
2024-10-14 18:29:27 +0300

    MR-3: Login test and changes to structure of POM

---
+++ POM/pages/login.page.ts
@@ -0,0 +1,26 @@
+import { Page } from '@playwright/test';
+import { BasePage } from '../base/base.page';
+import { WebClientConfig } from '../index.page';
+
+export class LoginPage extends BasePage {
+  public readonly userNameInput = this.page.getByTestId('username');
+  public readonly passwordInput = this.page.getByTestId('password');
+  public readonly loginButton = this.page.getByTestId('signin');
+
+  constructor(
+    page: Page,
+    private readonly config: WebClientConfig
+  ) {
+    super(page);
+  }
+
+  public async openLogin(): Promise<void> {
+    await this.page.goto(`${this.config.url}/login`);
+  }
+
+  public async login(username: string, password: string): Promise<void> {
+    await this.userNameInput.fill(username);
+    await this.passwordInput.fill(password);
+    await this.loginButton.click();
+  }
+}
--- tests/sign_in.spec.ts
+++
@@ -1,5 +0,0 @@
-import { baseFixture as test } from './main.fixture';
-
-test('T1: Sign in', async ({ testWebClient }) => {
-  await testWebClient.loginPage.openLogin();
-});
